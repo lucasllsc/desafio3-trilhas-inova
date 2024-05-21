2d8feefd026cb21ac875dc69fa22b5b41e187077
Lucas Leonardo <93744367+lucasllsc@users.noreply.github.com>
2024-05-21 20:21:18 -0300

    Add files via upload

--- js/digitacaoNome.js
+++ js/digitacaoNome.js
@@ -1,16 +1,30 @@
-var atraso = 200;
-var textoElemento = document.querySelector(".apresentacao__conteudo__titulo");
+var atraso = 100;
+var textoElemento = document.querySelector(".digitacao");
 let titulo = textoElemento.textContent;
 textoElemento.textContent = '';
+var palavras = titulo.split(" ");
 
 var contadorDeLetras = 0;
+var palavraAtual = 0;
+
 function escreverTexto() {
-  if (contadorDeLetras < titulo.length) {
-    textoElemento.textContent += titulo.charAt(contadorDeLetras);
-    contadorDeLetras++;
-    
-    setTimeout(escreverTexto, atraso);
+  if (palavraAtual < palavras.length){
+    if (contadorDeLetras < palavras[palavraAtual].length) {
+      if (palavraAtual === palavras.length - 1) {
+        textoElemento.innerHTML += `<span class="titulo-destaque">${palavras[palavraAtual].charAt(contadorDeLetras)}</span>`;
+      } else {
+        textoElemento.innerHTML += palavras[palavraAtual].charAt(contadorDeLetras);
+      }
+      contadorDeLetras++;
+      setTimeout(escreverTexto, atraso);
+    } else {
+      textoElemento.innerHTML += " ";
+      palavraAtual++;
+      contadorDeLetras = 0;
+      setTimeout(escreverTexto, atraso);
+    }
   }
+  
 }
 
 escreverTexto();
