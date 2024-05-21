ea090247afca1be00d474d08513062ea2b709b56
Lucas Leonardo <93744367+lucasllsc@users.noreply.github.com>
2024-05-21 16:14:02 -0300

    Add files via upload

--- js/digitacaoNome.js
+++ js/digitacaoNome.js
@@ -1,12 +1,12 @@
-var nome = "Lucas Campos";
 var atraso = 200;
 var textoElemento = document.querySelector(".apresentacao__conteudo__titulo");
-
+let titulo = textoElemento.textContent;
+textoElemento.textContent = '';
 
 var contadorDeLetras = 0;
 function escreverTexto() {
-  if (contadorDeLetras < nome.length) {
-    textoElemento.textContent += nome.charAt(contadorDeLetras);
+  if (contadorDeLetras < titulo.length) {
+    textoElemento.textContent += titulo.charAt(contadorDeLetras);
     contadorDeLetras++;
     
     setTimeout(escreverTexto, atraso);
